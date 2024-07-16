c4ee643b6b2eb4ffd18b0547ac644aea4d41a56b
Gabriel Medeiros <gabrielmedeirosn05@gmail.com>
2024-07-16 08:02:12 -0300

    Day 2 - Building Contract

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
@@ -6,11 +7,28 @@ const tokens = (n) => {
 
 describe("ETHDaddy", () => {
 
-  it('name', async () => {
+  let ethDeploy; 
+
+  const NAME = "ETH Daddy"
+  const SYMBOL = "ETHD"
+
+  beforeEach( async () => {
+    const signers = await ethers.getSigners()
+
     const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
-    let ethDeploy = await ETHDaddy.deploy()
-    const result = await ethDeploy.names
-    expect(result).to.equal('ETH Daddy')
+    ethDeploy = await ETHDaddy.deploy('ETH Daddy', 'ETHD')
+  })
+
+  describe("Deployment", () => {
+    it('name test', async () => {
+      const result = await ethDeploy.name()
+      expect(result).to.equal(NAME)
+    })
+  
+    it('symbol test', async () => {
+      const result = await ethDeploy.symbol()
+      expect(result).to.equal(SYMBOL)
+    })
   })
 
 })
