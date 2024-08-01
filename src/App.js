7d14e4d6b21b18bc47a13024511fbd2bea7fecb2
Gabriel Medeiros <gabrielmedeirosn05@gmail.com>
2024-08-01 06:00:27 -0300

    Day 9 - Setting the List

--- src/App.js
+++ src/App.js
@@ -13,12 +13,39 @@ import ETHDaddy from './abis/ETHDaddy.json'
 import config from './config.json';
 
 function App() {
-
+  const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null)
+
+  const [ethDaddy, setETHDaddy] = useState(null)
+  const [domains, setDomains] = useState([])
 
   const loadBlockchainData = async () =>
   {
-    
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+    console.log(network)
+
+    const ethDaddy = new ethers.Contract(config[network.chainId].ETHDaddy.address, ETHDaddy, provider)
+    setETHDaddy(ethDaddy)
+
+    const maxSupply = await ethDeploy.maxSupply()
+    const domains = []
+
+    for(var i = 1; 1 <= maxSupply; i++)
+    {
+      const domain = await ethDaddy.getDomain(i)
+      domains.push(domain)
+    }
+
+    setDomains(domains)
+
+    window.ethereum.on('accountsChanged',async() => {
+      const accounts = await window.ethereum.request({'method': 'eth_requestAccounts'})
+      const account = ethers.utils.getAddress(accounts[0])
+      console.log(account)
+    })
   }
 
   useEffect(() => {
@@ -34,7 +61,19 @@ function App() {
 
       <div className='cards__section'>
 
-        <h2 className='cards__title'>Welcome to ETH Daddy!</h2>
+        <h2 className='cards__title'>The reason why you need a domain:</h2>
+
+        <p className='cards__description'>
+          Have your own custom name, use it across different places around the web and store all your data.
+        </p>
+
+        <hr/>
+
+        <div className='cards__list'>
+          {domains.map((domain, index) => (
+            <Domain domain={domain} ethDaddy={ethDaddy} provider={provider} id={index + 1} key={index}/>
+          ))}
+        </div>
 
       </div>
 
